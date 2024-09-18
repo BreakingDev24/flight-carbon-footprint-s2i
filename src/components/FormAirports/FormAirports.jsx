76934dc7d0257fbb4d0b73eb44578d2f472a9ea3
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-18 17:36:53 +0200

    created form structure
    -added Departure, Arrival, Passengers input

---
+++ src/components/FormAirports/FormAirports.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import style from "./FormAirports.module.css";
+
+export default function FormInput({ onSubmit }) {
+  const [formData, setFormaData] = useState({
+    departure: "",
+    arrival: "",
+    passengers: 1,
+  });
+
+  return (
+    <div className={style.formContainer}>
+      <form /* onSubmit={handleSubmit} */>
+        <div className={style.inputContainer}>
+          <label htmlFor="departure">Departure:</label>
+          <input
+            id="daparture"
+            name="departure"
+            list="airports"
+            value={formData.departure}
+            // onChange={handleInputChange}
+            required
+            autoComplete="off"
+          />
+        </div>
+
+        <div className={style.inputContainer}>
+          <label htmlFor="arrival">Arrival:</label>
+          <input
+            id="arrival"
+            name="arrival"
+            list="airports"
+            value={formData.departure}
+            // onChange={handleInputChange}
+            required
+            autoComplete="off"
+          />
+        </div>
+
+        <div className={style.inputNumberContainer}>
+          <label htmlFor="passengers">Passengers:</label>
+          <input
+            type="number"
+            id="passengers"
+            name="passengers"
+            value={formData.passengers}
+            // onChange={handleInputChange}
+          />
+        </div>
+
+        <button type="submit">Calculate FootPrint</button>
+      </form>
+    </div>
+  );
+}

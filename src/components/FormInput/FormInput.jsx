d0814c5f4f9473c019bbfca757d0b572d2e47543
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-19 10:38:53 +0200

    Fix airport data selection logic

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import style from "./FormInput.module.css";
 
-export default function FormInput({ onSubmit }) {
-  const [formData, setFormaData] = useState({
+export default function FormInput({ airportCode, setAirportCode }) {
+  const [formData, setFormData] = useState({
     departure: "",
     arrival: "",
     passengers: 1,
@@ -24,6 +24,7 @@ export default function FormInput({ onSubmit }) {
       } catch (error) {
         console.error(error);
       }
+      // console.log(airportData);
     };
 
     fetchAirports();
@@ -32,7 +33,7 @@ export default function FormInput({ onSubmit }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
-    setFormaData((prevData) => ({
+    setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
@@ -43,12 +44,12 @@ export default function FormInput({ onSubmit }) {
 
     const airport = airportData.find(
       (airport) =>
-        `${airport.code} - ${airport.name} ${airport.city}(${airport.country})` ===
+        `${airport.code} - ${airport.name} - ${airport.city}(${airport.country})` ===
         value
     );
 
     if (airport) {
-      setFormaData((prevFormData) => ({
+      setAirportCode((prevFormData) => ({
         ...prevFormData,
         [field]: airport.code,
       }));
@@ -58,76 +59,74 @@ export default function FormInput({ onSubmit }) {
       ...prevSelected,
       [field]: value,
     }));
-    console.log(selectedAirport);
+    // console.log(selectedAirport);
   };
 
   return (
-    <div className={style.formContainer}>
-      <form /* onSubmit={handleSubmit} */>
-        <div className={style.inputContainer}>
-          <label htmlFor="departure">Departure:</label>
-          <input
-            id="daparture"
-            name="departure"
-            list="airports"
-            value={selectedAirport.departure}
-            onChange={(e) => handleInputChange(e)}
-            onInput={(e) => handleSelectedAirport(e, "departure")}
-            required
-            autoComplete="off"
-          />
+    <>
+      <div className={style.inputContainer}>
+        <label htmlFor="departure">Departure:</label>
+        <input
+          id="daparture"
+          name="departure"
+          list="airports"
+          value={selectedAirport.departure}
+          onChange={(e) => handleInputChange(e)}
+          onInput={(e) => handleSelectedAirport(e, "departure")}
+          required
+          autoComplete="off"
+        />
 
-          <datalist id="airports">
-            {airportData &&
-              airportData.map((airport) => (
-                <option
-                  key={airport.code}
-                  value={`${airport.code} - ${airport.name} - ${airport.city} (${airport.country})`}
-                >
-                  {airport.code} - {airport.name} - {airport.city}(
-                  {airport.country})
-                </option>
-              ))}
-          </datalist>
-        </div>
+        <datalist id="airports">
+          {airportData &&
+            airportData.map((airport) => (
+              <option
+                key={airport.code}
+                value={`${airport.code} - ${airport.name} - ${airport.city}(${airport.country})`}
+              >
+                {airport.code} - {airport.name} - {airport.city}(
+                {airport.country})
+              </option>
+            ))}
+        </datalist>
+      </div>
 
-        <div className={style.inputContainer}>
-          <label htmlFor="arrival">Arrival:</label>
-          <input
-            id="arrival"
-            name="arrival"
-            list="airports"
-            value={selectedAirport.arrival}
-            onChange={handleInputChange}
-            onInput={(e) => handleSelectedAirport(e, "arrival")}
-            required
-            autoComplete="off"
-          />
+      <div className={style.inputContainer}>
+        <label htmlFor="arrival">Arrival:</label>
+        <input
+          id="arrival"
+          name="arrival"
+          list="airports"
+          value={selectedAirport.arrival}
+          onChange={handleInputChange}
+          onInput={(e) => handleSelectedAirport(e, "arrival")}
+          required
+          autoComplete="off"
+        />
 
-          <datalist id="airports">
-            {airportData &&
-              airportData.map((airport) => (
-                <option key={airport.code} value={airport.code}>
-                  {airport.code} - {airport.name} - {airport.city}(
-                  {airport.country})
-                </option>
-              ))}
-          </datalist>
-        </div>
+        <datalist id="airports">
+          {airportData &&
+            airportData.map((airport) => (
+              <option key={airport.code} value={airport.code}>
+                {airport.code} - {airport.name} - {airport.city}(
+                {airport.country})
+              </option>
+            ))}
+        </datalist>
+      </div>
 
-        <div className={style.inputNumberContainer}>
-          <label htmlFor="passengers">Passengers:</label>
-          <input
-            type="number"
-            id="passengers"
-            name="passengers"
-            value={formData.passengers}
-            onChange={handleInputChange}
-          />
-        </div>
+      <div className={style.inputNumberContainer}>
+        <label htmlFor="passengers">Passengers:</label>
+        <input
+          type="number"
+          id="passengers"
+          name="passengers"
+          value={formData.passengers}
+          onChange={handleInputChange}
+        />
+      </div>
 
-        <button type="submit">Calculate FootPrint</button>
-      </form>
-    </div>
+      <button type="submit">Calculate FootPrint</button>
+    </>
   );
 }

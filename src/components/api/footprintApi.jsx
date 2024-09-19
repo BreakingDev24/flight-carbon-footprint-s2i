5a8329d5dbd49ee80c5221048dadd2145c8e1cb6
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-20 01:34:02 +0200

    feat: add calculateFootprint function for flight footprint calculation
    - Implemented `calculateFootprint` using Axios to retrieve flight footprint data from the GoClimate API.

---
+++ src/components/api/footprintApi.jsx
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const calculateFootprint = async ({ departure, arrival }) => {
+  const response = await axios.get(
+    "https://api.goclimate.com/v1/flight_footprint",
+    {
+      auth: {
+        username: import.meta.env.VITE_GOCLIMATE_API_KEY,
+      },
+      params: {
+        "segments[0][origin]": departure,
+        "segments[0][destination]": arrival,
+        cabin_class: "economy",
+      },
+    }
+  );
+  return response.data.footprint;
+};

b88b193fc60c8760c9e13766f1427674e284b92d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-20 01:40:24 +0200

    refactor: utilize React Query for footprint calculation
    - Replaced manual data fetching with React Query's `useQuery` hook for improved state management.
    - Created a `calculateFootprint` function for API requests to get flight footprint data.
    - Implemented conditional fetching to avoid unnecessary requests when airports are not provided.
    - Enhanced loading and error handling using React Query features.
    - Cleaned up the component by removing unnecessary state management for footprint data.

--- src/components/FootprintCalculator/FootprintCalculator.jsx
+++ src/components/FootprintCalculator/FootprintCalculator.jsx
@@ -1,34 +1,27 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import style from "./FootprintCalculator.module.css";
+import { useQuery } from "@tanstack/react-query";
+import { calculateFootprint } from "../api/footprintApi";
+
 export default function FootprintCalculator({
   departureAirport,
   arrivalAirport,
   passengers,
 }) {
-  const [footprint, setFootprint] = useState(null);
-
-  useEffect(() => {
-    const fetchFootrpint = async () => {
-      try {
-        const url = `https://api.goclimate.com/v1/flight_footprint`;
-        const response = await axios.get(url, {
-          auth: {
-            username: import.meta.env.VITE_GOCLIMATE_API_KEY,
-          },
-          params: {
-            "segments[0][origin]": departureAirport,
-            "segments[0][destination]": arrivalAirport,
-            cabin_class: "economy",
-          },
-        });
-        setFootprint(response.data.footprint);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchFootrpint();
-  }, [departureAirport, arrivalAirport]);
+  const {
+    data: footprint,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["footprint", departureAirport, arrivalAirport],
+    queryFn: () =>
+      calculateFootprint({
+        departure: departureAirport,
+        arrival: arrivalAirport,
+      }),
+    enabled: !!departureAirport && !!arrivalAirport,
+  });
 
   return (
     <div className={style.footprintContainer}>

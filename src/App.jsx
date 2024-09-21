b1eb933be6a827fbd5216a9d7b0ae0a8dff997aa
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-21 22:59:46 +0200

    feat: added react-router with basic routing and integrated Navbar and Footer
    - Added `BrowserRouter` from react-router-dom to enable routing.
    - Integrated `Navbar` and `Footer` components into the app layout.
    - Created a basic route for the Home page with the path `/`.
    - Wrapped the app with `AppLayoutContainer` and updated the structure to include navigation and footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayoutContainer from "./components/AppLayoutContainer/AppLayoutContainer";
+import Footer from "./components/Footer/Footer";
 // import FootprintForm from "./components/FootprintForm/FootprintForm";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <AppLayoutContainer>
-        <Home />
-      </AppLayoutContainer>
+      <BrowserRouter>
+        <AppLayoutContainer>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </AppLayoutContainer>
+      </BrowserRouter>
     </>
   );
 }

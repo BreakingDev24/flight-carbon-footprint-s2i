7b349aa44d4c243818b5a88496c7d541525c6a00
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-21 22:58:27 +0200

    feat: added Material-UI theme provider to root configuration
    - Integrated `ThemeProvider` from Material-UI to apply a default theme across the app.
    - Updated the root render to wrap the app in `ThemeProvider` for consistent styling with Material-UI.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
 import App from "./App.jsx";
 import "./index.css";
 
 const queryClient = new QueryClient();
+const theme = createTheme(); // Crea un tema di default
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );

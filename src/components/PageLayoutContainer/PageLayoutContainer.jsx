7c2d900d4898a23783aa45180a34a6f17762b861
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-24 00:38:01 +0200

    feat: enhance PageLayoutContainer for dynamic navbar height
    - Introduced state management to dynamically adjust layout based on navbar height.
    - Utilized useEffect to calculate and set the navbar height on window load.
    - Replaced the static CSS-based container with MUI Box for better styling and responsiveness.
    - The main content area is now adjusted to avoid overlap with the navbar, ensuring a cleaner layout.

--- src/components/PageLayoutContainer/PageLayoutContainer.jsx
+++ src/components/PageLayoutContainer/PageLayoutContainer.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
 import style from "./PageLayoutContainer.module.css";
+import { Container, Box } from "@mui/material";
 
 export default function PageLayoutContainer({ children }) {
-  return <main className={style.pageContainer}>{children}</main>;
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    const updateNavbarHeight = () => {
+      const navbar = document.querySelector(".navbar");
+      if (navbar) {
+        setNavbarHeight(navbar.offsetHeight);
+      }
+    };
+    window.onload = updateNavbarHeight();
+  }, []);
+  return (
+    <Box
+      component="main"
+      sx={{
+        flex: 1,
+        marginTop: `${navbarHeight}px`,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+      }}
+    >
+      {children}
+    </Box>
+  );
 }

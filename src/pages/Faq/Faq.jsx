182ac22db1835ba1ceb9be59fc61bebe6d1685c3
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-23 23:54:49 +0200

    feat: create FAQ page for Travel Green application
    - Implemented `Faq` page to address common questions regarding carbon footprints of flights.
    - Used Material-UI components such as `Accordion`, `AccordionSummary`, and `AccordionDetails` for a collapsible FAQ layout.
    - Added three FAQs

---
+++ src/pages/Faq/Faq.jsx
@@ -0,0 +1,45 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PageLayoutContainer from "../../components/PageLayoutContainer/PageLayoutContainer";
+export default function Faq() {
+  const faqs = [
+    {
+      question: "What is the carbon footprint of a flight?",
+      answer:
+        "The carbon footprint of a flight refers to the amount of greenhouse gases, mainly carbon dioxide (CO₂), emitted into the atmosphere as a result of the flight. This calculation takes into account fuel consumption, the distance traveled, and other factors that contribute to environmental pollution.",
+    },
+    {
+      question: "How can I reduce my carbon footprint when flying?",
+      answer:
+        "To reduce your carbon footprint, you can choose direct flights, fly with airlines that use more fuel-efficient aircraft, travel in economy class (where emissions are shared among more passengers), or offset your emissions by supporting carbon reduction projects.",
+    },
+    {
+      question:
+        "How does calculating my carbon footprint help the environment?",
+      answer:
+        "By knowing your carbon footprint, you can make more informed decisions when traveling. You can assess the environmental impact of your transportation choices and take steps to offset or reduce your emissions, contributing to mitigating global warming and the climate crisis.",
+    },
+  ];
+
+  return (
+    <PageLayoutContainer>
+      <div>
+        {faqs.map((faq, index) => (
+          <Accordion key={index}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h5">{faq.question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{faq.answer}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </PageLayoutContainer>
+  );
+}

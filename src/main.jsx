cc1075107cb64284da44ad6eaa217842653525e7
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-24 00:48:05 +0200

    refactor: update index.jsx to use custom theme and CssBaseline
    - Imported the custom theme from "./components/theme/theme.jsx" to enhance visual consistency.
    - Added the CssBaseline component to normalize CSS styles and improve overall presentation.
    - Removed the comment related to creating the default theme.

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import App from "./App.jsx";
 import "./index.css";
-
+import theme from "./components/theme/theme.jsx";
 const queryClient = new QueryClient();
-const theme = createTheme(); // Crea un tema di default
+// const theme = createTheme(); // Crea un tema di default
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </QueryClientProvider>

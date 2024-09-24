9147076cf7618325b38e4c49fc7dcf7d8c0b44b3
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-24 20:03:46 +0200

    fix: Adjust PageLayoutContainer to account for both navbar and footer height
    - Added logic to calculate and set footer height dynamically using `useEffect`.
    - Updated the `minHeight` of the container to ensure it occupies the full viewport height, minus the heights of both the navbar and footer.
    - The container now dynamically adjusts padding for better layout management across different screen sizes.

--- src/components/PageLayoutContainer/PageLayoutContainer.jsx
+++ src/components/PageLayoutContainer/PageLayoutContainer.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
-import style from "./PageLayoutContainer.module.css";
 import { Container, Box } from "@mui/material";
 
 export default function PageLayoutContainer({ children }) {
   const [navbarHeight, setNavbarHeight] = useState(0);
-
+  const [footerHeight, setFooterHeight] = useState(0);
   useEffect(() => {
     const updateNavbarHeight = () => {
       const navbar = document.querySelector(".navbar");
@@ -15,17 +14,24 @@ export default function PageLayoutContainer({ children }) {
     };
     window.onload = updateNavbarHeight();
   }, []);
+
+  useEffect(() => {
+    const footer = document.getElementById("footer");
+    if (footer) {
+      setFooterHeight(footer.offsetHeight);
+    }
+  }, []);
   return (
     <Box
       component="main"
       sx={{
-        flex: 1,
-        marginTop: `${navbarHeight}px`,
+        minHeight: `calc(100vh - ${footerHeight}px)`,
+        paddingTop: `${navbarHeight}px`,
+        paddingBottom: "20px",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        flex: 1,
       }}
     >
       {children}

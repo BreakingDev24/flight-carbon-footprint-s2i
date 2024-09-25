6c4083aff563f93d58bfd6a7f43daea182c389a6
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-25 17:23:32 +0200

    refactor: Integrate FootprintCalculator with FootprintContext
    - Updated the `FootprintCalculator` component to utilize the `useFootprint` hook from the FootprintContext, allowing for centralized state management.
    - Removed direct props for `departureAirport`, `arrivalAirport`, and `passengers`, instead relying on the context to obtain flight data.
    - The component now handles loading and error states from the context, improving user experience by providing more reactive feedback.
    - Cleaned up code to streamline functionality and ensure that footprint calculations are directly tied to the context-managed state.
    - This refactor enhances the maintainability and reusability of the component by aligning it with the application's state management strategy.

--- src/components/FootprintCalculator/FootprintCalculator.jsx
+++ src/components/FootprintCalculator/FootprintCalculator.jsx
@@ -1,36 +1,17 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import style from "./FootprintCalculator.module.css";
-import { useQuery } from "@tanstack/react-query";
-import { calculateFootprint } from "../api/footprintApi";
-
-export default function FootprintCalculator({
-  departureAirport,
-  arrivalAirport,
-  passengers,
-}) {
-  const {
-    data: footprint,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["footprint", departureAirport, arrivalAirport],
-    queryFn: () =>
-      calculateFootprint({
-        departure: departureAirport,
-        arrival: arrivalAirport,
-      }),
-    enabled: !!departureAirport && !!arrivalAirport,
-  });
+import { useFootprint } from "../context/footprintContext";
+export default function FootprintCalculator() {
+  const { footprint, isLoading, error, flightData } = useFootprint();
 
   return (
     <div className={style.footprintContainer}>
       {footprint ? (
         <div className={style.resultsContainer}>
           <p className={style.footprint}>
-            <span>Footprint:</span> <span>{footprint * passengers} kg CO2</span>
+            <span>Footprint:</span>{" "}
+            <span>{footprint * flightData.passengers} kg CO2</span>
           </p>
-          {passengers > 1 && (
+          {flightData.passengers > 1 && (
             <p className={style.footprintPassengers}>
               Footprints per passenger: {footprint} kg CO2
             </p>

fe5526a4a53b964a7815279c2920b4effc5b88c8
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-26 20:09:38 +0200

    feat: added helmet provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import { FootprintProvider } from "./components/context/footprintContext.jsx";
 import { AirportProvider } from "./components/context/airportContext.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -18,7 +19,9 @@ createRoot(document.getElementById("root")).render(
         <FootprintProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <App />
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
           </ThemeProvider>
         </FootprintProvider>
       </AirportProvider>

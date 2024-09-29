3a56df293c1c7fa613cc3181fcea523fc1da0c67
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-09-29 02:16:21 +0200

    refactor: - Replaced FootprintProvider with FlightDataProvider in the main application render.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
-import { FootprintProvider } from "./components/context/footprintContext.jsx";
+import { FlightDataProvider } from "./components/context/flightDataContext.jsx";
 import { AirportProvider } from "./components/context/airportContext.jsx";
 import { HelmetProvider } from "react-helmet-async";
 
@@ -15,14 +15,14 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AirportProvider>
-        <FootprintProvider>
+        <FlightDataProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <HelmetProvider>
               <App />
             </HelmetProvider>
           </ThemeProvider>
-        </FootprintProvider>
+        </FlightDataProvider>
       </AirportProvider>
     </QueryClientProvider>
   </StrictMode>
